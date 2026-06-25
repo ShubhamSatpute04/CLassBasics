e61bcdd69af4a007bb7b9bc6ef4491994efe1834
ShubhamSatpute04 <shubhamsatpute1498@gmail.com>
2026-06-25 20:30:39 +0530

    add multiplication code

--- sum.py
+++ sum.py
@@ -5,3 +5,11 @@ num1=int(input('enter num1'))
 num2=int(input('enter num2'))
 
 print(sum(num1+num2))
+
+def multi(num1,num2):
+	print(num1 * num2)
+
+num1=int(input('enter num1'))
+num2=int(input('enter num2'))
+
+print(sum(num1+num2))
