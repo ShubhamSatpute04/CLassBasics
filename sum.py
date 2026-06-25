e851a2b9d6a802d17591088c4f52f46b25120bf2
Shubham Satpute <shubhamsatpute1498@gmail.com>
2026-06-25 20:32:57 +0530

    add subtraction code

--- sum.py
+++ sum.py
@@ -13,3 +13,11 @@ num1=int(input('enter num1'))
 num2=int(input('enter num2'))
 
 print(sum(num1+num2))
+
+def subtract(num1,num2):
+	print(num1 - num2)
+
+num1=int(input('enter num1'))
+num2=int(input('enter num2'))
+
+print(sum(num1+num2))
